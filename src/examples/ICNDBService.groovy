f28b63361351bce71dc6f8f50c49be59dbb9f199
ashishbhatnagar05 <ashishbhatnagar05@gmail.com>
2018-04-23 00:25:40 +0530

    ProjectEuler
    2 of project euler solution, need to improve the prime calculation

--- src/examples/ICNDBService.groovy
+++ src/examples/ICNDBService.groovy
@@ -5,7 +5,7 @@ class ICNDBService{
 
 	void getJokes() {
 		String base='http://api.icndb.com/jokes/random?'
-		def qs=[firstName:'ashish',lastName:'bhatnagar'].collect{ k,v -> "$k=$v" }.join('&')
+		def qs=[limitTo:'[nerdy]',firstName:'ashish',lastName:'bhatnagar'].collect{ k,v -> "$k=$v" }.join('&')
 		String result="$base$qs".toURL().text
 		def json=new JsonSlurper().parseText(result)
 		println json.value.joke
